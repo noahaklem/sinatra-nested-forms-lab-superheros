43c32e028a15ac3f850a6968464ecb0cd77c7c46
Noah A Klem <noahaklem@gmail.com>
2021-09-23 10:47:26 -0600

    commit tree 309f6697df19ed585a110a534282cb026d6b0215

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/team' do
+        @team = Team.new(params[:team])
+
+        params[:team][:superheroes].each do |sh|
+            Superhero.new(sh)
+        end
+
+        @superheroes = Superhero.all
+
+      erb :team
+    end
 end
